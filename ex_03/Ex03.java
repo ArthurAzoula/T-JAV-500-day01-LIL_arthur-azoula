87b3f7d3606e596dd75a0cdbbd71cde7b89967b0
Arthur Azoula <arthur.azoula@epitech.eu>
2023-10-23 10:50:16 +0200

    add exo3

---
+++ ex_03/Ex03.java
@@ -0,0 +1,29 @@
+package ex_03;
+
+import java.util.ArrayList;
+
+public class Ex03 {
+
+    public static void printArray(ArrayList<String> myArray) {
+
+        for (String s : myArray) {
+            System.out.println(s);
+        }
+
+    }
+
+    /*
+
+    public static void main(String[] args)  {
+
+            ArrayList<String> myArray = new ArrayList<String>();
+
+            myArray.add("Volvo");
+            myArray.add("BMW");
+            myArray.add("Ford");
+
+            printArray(myArray);
+    }
+
+     */
+}
