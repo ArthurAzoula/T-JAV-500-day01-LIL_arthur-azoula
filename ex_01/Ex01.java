d1ee84c07f3213bb9beacb62581b1a6ffac2d816
Arthur Azoula <arthur.azoula@epitech.eu>
2023-10-23 10:31:29 +0200

    Add first exo

---
+++ ex_01/Ex01.java
@@ -0,0 +1,15 @@
+package ex_01;
+
+public class Ex01 {
+
+    public static void myConcat(String s1, String s2) {
+        System.out.println(s1 + ' ' + s2);
+    }
+
+    /*
+    public static void main(String[] args) {
+        myConcat("hello", "world");
+    }
+    */
+
+}
